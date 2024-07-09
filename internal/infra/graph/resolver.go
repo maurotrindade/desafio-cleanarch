54e7b58a81f2e6ef54159c8e336eee4c07bfd216
Mauro Trindade <mauro.trindade@invillia.com>
2024-07-09 16:22:16 -0300

    feat: create new query and genarate code

--- internal/infra/graph/resolver.go
+++ internal/infra/graph/resolver.go
@@ -8,4 +8,5 @@ import "github.com/maurotrindade/desafio-cleanarch/internal/usecase"
 
 type Resolver struct {
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrderUseCase   usecase.ListOrderUseCase
 }
